2404804e241e2daab1b72b9ba04648eb58c68e6f
Shashank Naik <shashanknaik808@gmail.com>
2023-07-27 10:24:19 +0530

    Refracting the nested loops in JS

--- scripts.js
+++ scripts.js
@@ -4,28 +4,38 @@ let hasFlippedCard = false;
 let firstCard, secondCard;
 
 function flipCard() {
-    this.classList.toggle('flip');
-    
-    if (!hasFlippedCard) {
-    
-        hasFlippedCard = true;
-        firstCard = this;
-    } else {
-    
-        hasFlippedCard = false;
-        secondCard = this;
-    
-        if (firstCard.dataset.framework === secondCard.dataset.framework) {
-            firstCard.removeEventListener('click', flipCard);
-            secondCard.removeEventListener('click', flipCard);
+  this.classList.toggle('flip');
 
-          } else {
-            setTimeout(() => {
-              firstCard.classList.remove('flip');
-              secondCard.classList.remove('flip');
-            }, 1500);
-          }
-    }
+  if (!hasFlippedCard) {
+
+    hasFlippedCard = true;
+    firstCard = this;
+
+    return;
+  }
+
+  hasFlippedCard = false;
+  secondCard = this;
+
+  checkForMatch();
+}
+
+function checkForMatch() {
+  let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
+
+  isMatch ? disableCards() : unflipCards();
+}
+
+function disableCards() {
+  firstCard.removeEventListener('click', flipCard);
+  secondCard.removeEventListener('click', flipCard);
+}
+
+function unflipCards() {
+  setTimeout(() => {
+    firstCard.classList.remove('flip');
+    secondCard.classList.remove('flip');
+  }, 1500);
 }
 
 
